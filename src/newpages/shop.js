c1126c0b626958d3eb48e979a1fbbd0ad50f0431
Rakesh Munukuntla <rakesh.munukuntla@cjsstechnologies.com>
2025-10-29 16:12:11 +0530

    Updated components and fixed bugs

--- src/newpages/shop.js
+++ src/newpages/shop.js
@@ -2,6 +2,8 @@ import './shop.css';
 import HeaderBottom from '../header/headerBottom';
 import HeaderTop from '../header/headerTop';
 import HeaderMid from '../header/headerMid';
+import FooterTop from '../footer/footertop';
+import MainFooter from '../footer/mainfooter';
 function Shop()
 {
     return(
@@ -33,16 +35,119 @@ function Shop()
                 </div>
                 
                 <div className='filterbyprice'>
-                    <h3>Filter by price</h3>
+                    <h3>FILTER BY PRICE</h3>
                     <hr></hr>
                     <input type="range" min="0" max="500"  />
+                       <div class="item">
+                       <button>Filter</button>
+                        <span>$0-$500</span>
+                        </div>
+                </div>
+                <div className='selectbycolor'>
+                    <h3>SELECT BY COLOR</h3>
+                    <hr></hr>
+                    <div class="item">
+                        <span>Black</span>
+                        <span>(6)</span>
+                    </div>
+                    <div class="item">
+                        <span>Blue</span>
+                        <span>(8)</span>
+                    </div>
+                      <div class="item">
+                        <span>Brown</span>
+                        <span>(10)</span>
+                    </div>
+                      <div class="item">
+                        <span>Green</span>
+                        <span>(6)</span>
+                    </div>
+                      <div class="item">
+                        <span>Pink</span>
+                        <span>(4)</span>
+                    </div>
+                </div>
+
+                <div className='selectbycolor'>
+                    <h3>SELECT BY SIZE</h3>
+                    <hr></hr>
+                    <div class="item">
+                        <span>S</span>
+                        <span>(6)</span>
+                    </div>
+                    <div class="item">
+                        <span>M</span>
+                        <span>(8)</span>
+                    </div>
+                      <div class="item">
+                        <span>L</span>
+                        <span>(10)</span>
+                    </div>
+                      <div class="item">
+                        <span>XL</span>
+                        <span>(6)</span>
+                    </div>
+                      <div class="item">
+                        <span>XL</span>
+                        <span>(4)</span>
+                    </div>
+                </div>
+
+
+                <div className='selectbycolor'>
+                    <h3>MANUFACTURER</h3>
+                    <hr></hr>
+                    <div class="item">
+                        <span>Break Parts</span>
+                        <span>(6)</span>
+                    </div>
+                    <div class="item">
+                        <span>Accessories</span>
+                        <span>(10)</span>
+                    </div>
+                      <div class="item">
+                        <span>Engine Parts</span>
+                        <span>(4)</span>
+                    </div>
+                      <div class="item">
+                        <span>Hermes</span>
+                        <span>(10)</span>
+                    </div>
+                      <div class="item">
+                        <span>Louis vuitton</span>
+                        <span>(8)</span>
+                    </div>
+                </div>
+
+
+                <div className='productstags'>
+                    <h3>PRODUCT TAGS</h3>
+                    <hr></hr>
+                    <div className='buts'>
+                        <button>Men</button>
+                        <button>Women</button>
+                        <button>Watches</button>
+                        <button>Bags</button>
+                        <button>Dress</button>
+                        <button>Belt</button>
+                        <button>Accessories</button>
+                        <button>Shoes</button>
+                    </div>
+                </div>
+
+                <div className='bannerofshop'>
+
                 </div>
                 
             </div>
             <div className='verticalPart2'>
 
             </div>
+            
         </div>
+        
+        <FooterTop></FooterTop>
+        <MainFooter></MainFooter>
         </div>
     )
 }
